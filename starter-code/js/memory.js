673ee158b2185dede0415a106c12b3277a1367d4
ingridpitta <ingridpitta056@gmail.com>
2020-02-06 20:48:14 -0300

    LAB | JS Memory Game ---> JS almost done, still have to do the shuffleCards method.[WIP]

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,55 +1,37 @@
 class MemoryGame {
   constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    this.pickedCards = pickedCards;
-    this.pairsClicked = pairsClicked;
-    this.pairsGuessed = pairsGuessed;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
   shuffleCards(cards) {
     if (!cards) {
       return undefined;
     }
 
-   
-
     for (let i = cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       let elements = [cards[j]];
-      return elements
+      return elements;
     }
   }
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  checkIfPair(card1, card2) {
+    if (card1) {
+      this.pairsClicked++;
+    }
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
-
-const cards1 = [
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "spiderman", img: "spiderman.jpg" },
-  { name: "superman", img: "superman.jpg" },
-  { name: "the avengers", img: "the-avengers.jpg" },
-  { name: "thor", img: "thor.jpg" },
-  { name: "aquaman", img: "aquaman.jpg" },
-  { name: "batman", img: "batman.jpg" },
-  { name: "captain america", img: "captain-america.jpg" },
-  { name: "fantastic four", img: "fantastic-four.jpg" },
-  { name: "flash", img: "flash.jpg" },
-  { name: "green arrow", img: "green-arrow.jpg" },
-  { name: "green lantern", img: "green-lantern.jpg" },
-  { name: "ironman", img: "ironman.jpg" },
-  { name: "spiderman", img: "spiderman.jpg" },
-  { name: "superman", img: "superman.jpg" },
-  { name: "the avengers", img: "the-avengers.jpg" },
-  { name: "thor", img: "thor.jpg" }
-];
-let teste = new MemoryGame();
-
-let card1 = teste.shuffleCards(cards1);
-
-console.log(card1);
