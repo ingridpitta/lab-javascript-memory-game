7ffc77fc09b2e5930d53ceb29d852e33ca2d3da2
ingridpitta <ingridpitta056@gmail.com>
2020-02-05 22:02:57 -0300

    LAB | JS Memory Game [WIP]

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,55 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = pickedCards;
+    this.pairsClicked = pairsClicked;
+    this.pairsGuessed = pairsGuessed;
   }
-  shuffleCards() {}
+  shuffleCards(cards) {
+    if (!cards) {
+      return undefined;
+    }
+
+   
+
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      let elements = [cards[j]];
+      return elements
+    }
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
+
+const cards1 = [
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" }
+];
+let teste = new MemoryGame();
+
+let card1 = teste.shuffleCards(cards1);
+
+console.log(card1);
